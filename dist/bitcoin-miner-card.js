060fb444f16bbaaf76974900f7bb35088aecc94b
daylehouse <dayle@daylehouse.co.uk>
2026-05-09 15:02:19 +0100

    resziing image assets and adding an overheat image for the miner card. Also updated the copy-assets script to include the new overheat image.

--- dist/bitcoin-miner-card.js
+++ dist/bitcoin-miner-card.js
@@ -154,7 +154,7 @@ const Vr={attribute:!0,type:String,converter:x,reflect:!1,hasChanged:_},Hr=(t=Vr
       position: absolute;
       left: 80.8%;
       top: 36.5%;
-      width: 55.5%;
+      width: 27.75%;
       height: auto;
       transform: translate(-50%, -50%);
       z-index: 5;
@@ -364,7 +364,7 @@ const Vr={attribute:!0,type:String,converter:x,reflect:!1,hasChanged:_},Hr=(t=Vr
       .overheat-indicator {
         left: 81.3%;
         top: 37%;
-        width: 66%;
+        width: 33%;
       }
 
       .title-value {
